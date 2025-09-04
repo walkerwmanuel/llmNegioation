702c602c505ab292dd69daa6dbaabb36266188db
walkerwmanuel <walkerwmanuel@gmail.com>
2025-09-04 11:32:05 -0400

    added test to test page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import TextToText from "./pages/TextToText";
 
 export default function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/text-to-text" element={<TextToText />} />
+      </Routes>
+    </Router>
+  );
 }
---
+++ frontend/src/pages/TextToText.tsx
@@ -0,0 +1,8 @@
+export default function TextToText() {
+    return (
+      <div style={{ textAlign: "center", marginTop: "5rem" }}>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+  
