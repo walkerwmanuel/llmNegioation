0e625a6457853532a3bbc1869ffc7b511b15f8ca
walkerwmanuel <walkerwmanuel@gmail.com>
2025-10-24 08:51:29 -0400

    added docker ability

--- backend/main.py
+++ backend/main.py
@@ -13,10 +13,10 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],    
-    allow_headers=["*"],   
+    allow_origins=["*"],
+    allow_credentials=False,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(negotiate_router)
